55d7af68aadc51a93b715bd835be69a04200352b
markom132 <markomarkoviccb@gmail.com>
2022-08-16 10:35:43 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "baonxxoll";
+        String s2 = "baoollnnololgbax";
+        String s3 = "qawabawaonl";
+        String s4 = "onlablabloon";
+        System.out.println(Method.call(s1));
+        System.out.println(Method.call(s2));
+        System.out.println(Method.call(s3));
+        System.out.println(Method.call(s4));
+    }
+}
